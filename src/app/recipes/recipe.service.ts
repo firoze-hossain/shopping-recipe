bf53dabb782d16ffb07db926ade9d5d051651aa5
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2023-01-05 22:21:05 +0600

    passing ingredients from recipes to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,25 +1,34 @@
-import { EventEmitter } from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
 import {Recipe} from "./recipe.model";
 import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
-  recipeSelected=new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe("A Test Recipe", "This is simple text case",
       "https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/2022-05/Tomato-Pie_0256.jpg?itok=c63mh-z9",
       [
-        new Ingredient('Meat',1),
-        new Ingredient('French Fries',20)
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
       ]),
     new Recipe("Another Test Recipe", "This is simple text case",
       "https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/2022-05/Tomato-Pie_0256.jpg?itok=c63mh-z9",
       [
-        new Ingredient('Buns',2),
-        new Ingredient('Meat',1)
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
       ])
   ];
 
+  constructor(private slService: ShoppingListService) {
+  }
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
